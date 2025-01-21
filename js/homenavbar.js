63e3038da1cac8831399da90456de6d296a1d046
hello world <159936375+tk22kalal2@users.noreply.github.com>
2025-01-21 23:26:34 +0530

    Update homenavbar.js

--- js/homenavbar.js
+++ js/homenavbar.js
@@ -5,3 +5,12 @@ const navLinks = document.querySelector('.nav-links');
 mobileMenu.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+// Search Bar Toggle
+const searchIcon = document.getElementById('search-icon');
+const searchContainer = document.getElementById('search-container');
+
+searchIcon.addEventListener('click', () => {
+  searchContainer.classList.toggle('active');
+  navLinks.classList.remove('active'); // Hide nav links when search bar is active
+});
